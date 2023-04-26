8553a86386bd554b1db27dc31a3ec37db6d79e60
Kyle Foral <kyleforal@gmail.com>
2023-04-26 22:17:19 +0000

    made all images load per slot and form to require all inputs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-function getPokemonData(name) {
+function getPokemonData(event) {
+  const curInputId = event.target.id;
+  // console.log('name', `img${curInputId}`);
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.pokemontcg.io/v2/cards?q=name:' + name);
+  xhr.open('GET', 'https://api.pokemontcg.io/v2/cards?q=!name:' + event.target.value);
   xhr.responseType = 'json';
   xhr.setRequestHeader('X-Api-Key', 'eeb741e2-0d06-436f-90ca-f25488d4f1d4');
   xhr.addEventListener('load', function () {
+
+    document.querySelector(`#img${curInputId}`).setAttribute('src', xhr.response.data[0].images.large);
+
     // console.log(xhr.status);
-    // console.log(xhr.response);
+    // console.log(xhr.response.data[0].images.large);
   });
   xhr.send();
 }
 
-getPokemonData('ditto');
+const $form = document.getElementById('card-pick');
+$form.addEventListener('change', getPokemonData);
