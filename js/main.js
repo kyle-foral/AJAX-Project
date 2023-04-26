c4a444098de9f951f9e414ba72ba072a739830f8
Kyle Foral <kyleforal@gmail.com>
2023-04-26 23:29:52 +0000

    cleaned up console logs, css and errors

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 function getPokemonData(event) {
   const curInputId = event.target.id;
-  // console.log('name', `img${curInputId}`);
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.pokemontcg.io/v2/cards?q=!name:' + event.target.value);
   xhr.responseType = 'json';
   xhr.setRequestHeader('X-Api-Key', 'eeb741e2-0d06-436f-90ca-f25488d4f1d4');
   xhr.addEventListener('load', function () {
-
     document.querySelector(`#img${curInputId}`).setAttribute('src', xhr.response.data[0].images.large);
-
-    // console.log(xhr.status);
-    // console.log(xhr.response.data[0].images.large);
   });
   xhr.send();
 }
@@ -24,12 +19,12 @@ function submitPokeball(event) {
   event.preventDefault();
   const entry = {
     entryId: data.nextEntryId,
-    name1: event.target.elements.pokemon1.value,
-    name2: event.target.elements.pokemon2.value,
-    name3: event.target.elements.pokemon3.value,
-    name4: event.target.elements.pokemon4.value,
-    name5: event.target.elements.pokemon5.value,
-    name6: event.target.elements.pokemon6.value
+    pokemon1: event.target.elements.pokemon1.value,
+    pokemon2: event.target.elements.pokemon2.value,
+    pokemon3: event.target.elements.pokemon3.value,
+    pokemon4: event.target.elements.pokemon4.value,
+    pokemon5: event.target.elements.pokemon5.value,
+    pokemon6: event.target.elements.pokemon6.value
   };
   data.entries.unshift(entry);
   for (let i = 1; i < $img.length; i++) {
@@ -37,4 +32,3 @@ function submitPokeball(event) {
   }
   $form.reset();
 }
-// console.log($form.elements.pokemon1.value);
